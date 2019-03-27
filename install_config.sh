ae82490fe1a8c610bfb82253f0f7f81dd212a4e3
Tomás Lungenstrass <tlungens@gmail.com>
2019-03-27 12:15:50 -0300

    update 27-03-2019

--- install_config.sh
+++ install_config.sh
@@ -5,8 +5,8 @@ ln -f ./config/exit_menu ~/.i3/exit_menu
 ln -f ./config/i3.conf ~/.i3/config
 ln -f ./config/gtkrc-2.0.mine.conf ~/.gtkrc-2.0.mine
 ln -f ./config/i3blocks.conf ~/.config/i3/i3blocks.conf
-ln -f ./config/sublime-keymap.conf ~'/.config/sublime-text-2/Packages/User/Default (Linux).sublime-keymap'
-ln -f ./config/sublime-settings.conf ~/.config/sublime-text-2/Packages/User/Preferences.sublime-settings
+ln -f ./config/sublime-keymap.conf ~'/.config/sublime-text-3/Packages/User/Default (Linux).sublime-keymap'
+ln -f ./config/sublime-settings.conf ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
 ln -f ./config/terminator.conf ~/.config/terminator/config
 ln -f ./config/xinitrc.conf ~/.xinitrc
 ln -f ./config/zprofile.conf ~/.zprofile
